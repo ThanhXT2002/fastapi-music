2be1f60cfdfa374f855787f7367c2825d31a6a4b
ThanhXT2002 <tranxuanthanhtxt2002@gmail.com>
2025-06-06 13:16:41 +0700

    change to postgresql

--- app/config/config.py
+++ app/config/config.py
@@ -12,8 +12,8 @@ class Settings(BaseSettings):
     # Base URL for serving static files
     BASE_URL: str = os.getenv("BASE_URL", "http://localhost:8000")
       
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./music.db")
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "your-secret-key-here")
+    DATABASE_URL: str = os.getenv("DATABASE_URL","")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "")
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
     
